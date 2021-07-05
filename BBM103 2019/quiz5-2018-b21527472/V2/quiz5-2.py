5ca27f555333be326c74be5799c5055def750473
MTUSTA <mtusta113@gmail.com>
2021-07-05 10:18:33 +0300

    update

---
+++ BBM103 2019/quiz5-2018-b21527472/V2/quiz5-2.py
@@ -0,0 +1,26 @@
+import sys
+
+girdi = int(sys.argv[1])
+
+def diamond(dongu, durum):
+    if durum == 0:
+        [print("", end=" ") for k in range(girdi-1-dongu)]
+        [print("*", end="") for i in range(dongu*2+1)]
+        if girdi != 1:
+            print()
+        if dongu == girdi-1:
+            durum += 1
+            dongu -= 1
+        dongu += 1
+    if durum == 1:
+        dongu -= 1
+        if dongu != -1:
+            [print("", end=" ") for j in range(girdi-1-dongu)]
+        [print("*", end="") for l in range(dongu*2+1)]
+        print()
+        if dongu <= 0:
+            durum += 1
+    if durum != 2:
+        diamond(dongu, durum)
+
+diamond(0,0)
---
+++ BBM103 2019/quiz5-2018-b21527472/quiz5-1.py
@@ -0,0 +1,31 @@
+girdi = input("Enter a number: ")
+girdi = int(girdi)
+
+
+def diamond(dongu, durum):
+    if durum == 0:
+        for k in range(girdi-1-dongu):
+            print("", end=" ")
+        for i in range(dongu*2+1):
+            print("*", end="")
+        if girdi != 1:
+            print()
+        if dongu == girdi-1:
+            durum += 1
+            dongu -= 1
+        dongu += 1
+    if durum == 1:
+        dongu -= 1
+        if dongu != -1:
+            for j in range(girdi-1-dongu):
+                print("", end=" ")
+        for l in range(dongu*2+1):
+            print("*", end="")
+        if girdi != 1 and dongu > 0:
+            print()
+        if dongu <= 0:
+            durum += 1
+    if durum != 2:
+        diamond(dongu, durum)
+
+diamond(0,0)
